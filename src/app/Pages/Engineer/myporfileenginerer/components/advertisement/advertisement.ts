00faf6f54878af1b03dcfe56ab6864b0328e8b89
mostafa hamed <Mostafa7ahmed>
2025-11-09 13:13:00 +0200

    feat: Enhance advertisement and suggestion components with responsive design and translation updates

--- src/app/Pages/Engineer/myporfileenginerer/components/advertisement/advertisement.ts
+++ src/app/Pages/Engineer/myporfileenginerer/components/advertisement/advertisement.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+import { ReactiveModeuls } from '../../../../../Shared/Modules/ReactiveForms.module';
 
 @Component({
   selector: 'app-advertisement',
-  imports: [],
+  imports: [ TranslatePipe, ReactiveModeuls],
   templateUrl: './advertisement.html',
   styleUrl: './advertisement.scss'
 })
 export class Advertisement {
+items = [
+  {
+    date: '14 أكتوبر, 2025',
+    price: 45,
+    title: 'اسم الاعلان',
+    discount: 10,
+    image: 'Image/aboutBG.jpg' // ضع مسار الصورة هنا
+  },
+    {
+    date: '14 أكتوبر, 2025',
+    price: 45,
+    title: 'اسم الاعلان',
+    discount: 10,
+    image: 'Image/aboutBG.jpg' // ضع مسار الصورة هنا
+  },
+    {
+    date: '14 أكتوبر, 2025',
+    price: 45,
+    title: 'اسم الاعلان',
+    discount: 10,
+    image: 'Image/aboutBG.jpg' // ضع مسار الصورة هنا
+  },
 
+  // كرر حسب الحاجة
+];
 }
